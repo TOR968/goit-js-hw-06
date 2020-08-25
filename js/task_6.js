f7331a284d8e7e4f8730ad15a1bb7a4e34273263
Maxim Turbal <tor968@yahoo.com>
2020-08-25 22:45:20 +0300

    1-6

--- js/task_6.js
+++ js/task_6.js
@@ -85,35 +85,20 @@ const users = [
   },
 ];
 // Write code under this line
-const getUserWithEmail = (array, mail) =>
-  array.find(({ email }) => email === mail);
+const getUsersWithAge = (array, min, max) =>
+  array.filter(({ age }) => age).map(({ name, email }) => ({}));
 
-console.log(getUserWithEmail(users, 'rossvazquez@xinware.com'));
+// console.log(getUsersWithAge(users, 20, 30));
+/* [
+    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
+    { name: 'Elma Head', email: 'elmahead@omatom.com' },
+    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
+] */
 
-/* {
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    isActive: false,
-    balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
-    age: 24,
-} */
-
-// console.log(getUserWithEmail(users, 'blackburndotson@furnigeer.com'));
-
-/* {
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
-    name: 'Blackburn Dotson',
-    email: 'blackburndotson@furnigeer.com',
-    eyeColor: 'brown',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-    isActive: false,
-    balance: 1498,
-    skills: ['non', 'amet', 'ipsum'],
-    gender: 'male',
-    age: 38,
-} */
+// console.log(getUsersWithAge(users, 30, 40));
+/* [
+    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
+    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
+    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
+    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
+] */
