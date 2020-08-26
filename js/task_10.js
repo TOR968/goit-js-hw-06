5ac63dbc696c0b42f83020d21472f9c73d82603a
Maxim <Tor968@yahoo.com>
2020-08-26 09:25:16 +0300

    10

--- js/task_10.js
+++ js/task_10.js
@@ -84,3 +84,17 @@ const users = [
     age: 39,
   },
 ];
+// Write code under this line
+const getSortedUniqueSkills = array => array;
+
+// console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
+ 'veniam' ]; */
